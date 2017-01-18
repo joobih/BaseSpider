a201030820c6900075972e402995148162b6ebaf
joobihjoobih <1006536507@qq.com>
2017-01-18 15:42:20 +0800

    add

--- useful.py
+++ useful.py
@@ -2,8 +2,10 @@
 # coding=utf-8
 
 import re  
+import hashlib
+import uuid
 from urlparse import urlparse
-
+from datetime import datetime
 
 """
     将url字符串参数进行检查,是否是合法的url字符串,True代表是,False代表否
@@ -31,4 +33,29 @@ def analysis_url(url):
         "path":path
     }
     return result
+
+
+"""
+    如果你想要一个完全不同的文件名不妨试试它,传入一个想要的文件类型
+"""
+def get_file_name(file_type):
+    uid = uuid.uuid4()
+    t = datetime.now()
+    t = str(t)
+    t = t[:19]
+    file_name = t + "_" + str(uid)
+    file_name = file_name.replace(" ","_").replace(".","_").replace(":","_").replace("-","_") + ".{type_}".format(type_ = file_type)
+    db_file = file_name
+    file_name = setting.SERVER_PATH + file_name
+    return db_file,file_name
+
+
+"""
+    将data字符串的数据转换为md5字符串
+"""
+def get_md5(data):
+    m2 = hashlib.md5()
+    m2.update(data)
+    md5_str = m2.hexdigest()
+    return md5_str
 
