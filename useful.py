72d0fc03b7e07fdc5bd0263eaec92a6ac2c2a79f
joobihjoobih <1006536507@qq.com>
2017-01-18 15:13:22 +0800

    useful

---
+++ useful.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+import urllib
+import re  
+from urlparse import urlparse
+      
+#    url = "http://www.baidu.com"  
+def is_normal_url(url):      
+
+    if re.match(r'^https?:/{2}\w.+$', url):  
+        print("Ok.")  
+        return True
+    else:  
+        print("Error.")  
+        return False
+
+def parser_url(url):
+    u = urlparse(url)
+    scheme = u.scheme
+    print "protocol:",scheme
+    hostname = u.hostname
+    print "hostname:",hostname
+    port = u.port
+    print "port:",port
+    path = u.path
+    print "path:",path
+    result = {
+        "scheme":scheme,
+        "hostname":hostname,
+        "port":port,
+        "path":path
+    }
+    return result
+
+url = "http://www.baidu.com"
+print is_normal_url(url)
+print parser_url(url)
