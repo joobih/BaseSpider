5263737e8af567c277ecda9992cfb32ca8daab30
joobihjoobih <1006536507@qq.com>
2017-02-06 17:43:30 +0800

    add headers

--- base_spider.py
+++ base_spider.py
@@ -15,7 +15,11 @@ class BaseSpider(object):
             self.s = session
         else:
             self.s = requests.Session()
-        self.headers = {"User-Agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36"}
+        self.headers = {
+            "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+            "User-Agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36",
+            "Accept-Encoding":"gzip, deflate, sdch"
+        }
 
         self.proxies = None
 
